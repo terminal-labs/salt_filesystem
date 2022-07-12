3397b954545e93240519b9ae9545c3c0e5e511ae
Michael Pinna <mikepinna3@gmail.com>
2022-07-12 16:58:42 -0400

    grain test

--- salt/_grains/env.py
+++ salt/_grains/env.py
@@ -4,8 +4,8 @@
 # flake8: noqa
 
 
-def env_grains():
+def env_grains(minion_id=__grains__['id']):
     # initialize a grains dictionary
-    minion_id = grains['id']
+    # minion_id = __grains__['id']
     test_grain = {"id2": minion_id}
     return test_grain
