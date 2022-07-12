c9795a763faeb2f78e236a07d4ba70cc23bed77d
Michael Pinna <mikepinna3@gmail.com>
2022-07-12 17:36:51 -0400

    grain test

--- salt/_grains/env.py
+++ salt/_grains/env.py
@@ -9,10 +9,14 @@ import socket
 def env_grains():
     # initialize a grains dictionary
     fqdn = socket.getfqdn()
+    # tiaa_grains = {
+    #     "tiaa_dc": fqdn[:2],
+    #     "tiaa_env" fqdn[3:4],
+    #     "tiaa_site": fqdn[5],
+    #     "tiaa_tier": fqdn[6]
+    # }
     tiaa_grains = {
-        "tiaa_dc": fqdn[:2],
-        "tiaa_env" fqdn[3:4],
-        "tiaa_site": fqdn[5],
-        "tiaa_tier": fqdn[6]
+        "foo1": fqdn,
+        "foo2": fqdn
     }
     return tiaa_grains
