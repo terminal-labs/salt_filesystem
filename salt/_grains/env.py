41a0f96e399455dcd8d59880d041c3fd941c9bdf
Michael Pinna <mikepinna3@gmail.com>
2022-07-12 17:40:26 -0400

    grain test

--- salt/_grains/env.py
+++ salt/_grains/env.py
@@ -12,7 +12,7 @@ def env_grains():
     tiaa_grains = {
         "tiaa_dc": fqdn[:3],
         "tiaa_env": fqdn[4:6],
-        "tiaa_site": fqdn[7],
-        "tiaa_tier": fqdn[8]
+        "tiaa_site": fqdn[6],
+        "tiaa_tier": fqdn[7]
     }
     return tiaa_grains
