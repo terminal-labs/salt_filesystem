5f215b248ab19e4bad2453cfdc790c6f400bc638
Michael Pinna <mikepinna3@gmail.com>
2022-07-12 16:53:09 -0400

    grain test

--- salt/_grains/env.py
+++ salt/_grains/env.py
@@ -3,10 +3,10 @@
 # type: ignore
 # flake8: noqa
 
-minion_id = __grains__['id']
+# minion_id = __grains__['id']
 
 
 def env_grains():
     # initialize a grains dictionary
-    test_grain = {"id2": minion_id}
+    test_grain = {"id2": "sam"}
     return test_grain
