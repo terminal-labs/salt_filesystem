66cc91eb1c79c2ea4040da616c04eb59fe6586f4
Michael Pinna <mikepinna3@gmail.com>
2022-07-13 05:17:40 -0400

    test state

--- salt/_modules/tiaa_key.py
+++ salt/_modules/tiaa_key.py
@@ -49,8 +49,8 @@ def generate_activation_key():
         'loc_Amazon_East': 'awe',
         'loc_Amazon_West': 'aww'}
     # Update default activation key with tiaa_loc
-    for activation_key, tiaa_grain in activation_key_map.items():
-        if tiaa_grains_dict['tiaa_dc'] in tiaa_grain:
-            activation_keys[0] = activation_key
+    # for activation_key, tiaa_grain in activation_key_map.items():
+    #     if tiaa_grains_dict['tiaa_dc'] in tiaa_grain:
+    #         activation_keys[0] = activation_key
 
     return activation_keys
