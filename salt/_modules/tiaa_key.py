4683ad655ce756dbe67ef4ba1e8a2849f73acc8b
Michael Pinna <mikepinna3@gmail.com>
2022-07-13 04:37:30 -0400

    test state

--- salt/_modules/tiaa_key.py
+++ salt/_modules/tiaa_key.py
@@ -7,18 +7,15 @@ Manage TIAA Activation Keys with Salt
 __virtualname__ = "tiaa_key"
 
 
-def get_tiaa_grains():
+def tiaa_grains():
     """
     Return a dictionary of TIAA prefixed grains.
     """
-    # tiaa_grains = {
-    #     "tiaa_dc": grains["tiaa_dc"],
-    #     "tiaa_env": grains["tiaa_env"],
-    #     "tiaa_site": grains["tiaa_site"],
-    #     "tiaa_tier": grains["tiaa_tier"]
-    # }
-    fqdn = __grains__['fqdn']
-    return fqdn
+    tiaa_dc = grains["tiaa_dc"],
+    tiaa_env = grains["tiaa_env"],
+    tiaa_site = grains["tiaa_site"],
+    tiaa_tier = grains["tiaa_tier"]
+    return dict(tiaa_dc=tiaa_dc, tiaa_env=tiaa_env, tiaa_site=tiaa_site, tiaa_tier=tiaa_tier)
 
 
 # def convert_hosted_zone_domain_name_to_id(keyid, key, domain_name):
