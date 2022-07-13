04fa354759ebb4c0368350e1d33746bc929aadeb
Michael Pinna <mikepinna3@gmail.com>
2022-07-13 05:49:44 -0400

    test state

--- salt/_modules/tiaa_key.py
+++ salt/_modules/tiaa_key.py
@@ -8,6 +8,9 @@ __virtualname__ = "tiaa_key"
 
 
 def hello(audience='world'):
+    """
+    Hello world example.
+    """
     return f'hello {audience}'
 
 
@@ -43,20 +46,53 @@ def generate_activation_key():
         '1-tier_UNKNOWN'
     ]
 
-    test = []
-
-    # TIAA prefixed custom grains with which to modify activation key.
-    tiaa_grains_dict = tiaa_grains()
-
     # Map tiaa_loc to activation key segment
     activation_key_map = {
         '1-loc_Charlotte': 'cha',
         '1-loc_Denver': 'den',
         'loc_Amazon_East': 'awe',
         'loc_Amazon_West': 'aww'}
-    # Update default activation key with tiaa_loc
+    # Update default activation key with tiaa_loc key
     for activation_key, tiaa_grain in activation_key_map.items():
         if __grains__["tiaa_dc"] in tiaa_grain:
             activation_keys[0] = activation_key
+
+    # Map tiaa_env to activation key segment
+    activation_key_map = {
+        '1-env_PD': 'pd',
+        '1-env_DR': 'dr',
+        '1-env_PF': 'pf',
+        '1-env_CT': 'ct',
+        '1-env_TS': 'ts',
+        '1-env_AT1': ['at', 'at1', 'ab'],
+        '1-env_DV': ['dv', 'd1', 'd2', 'd3', 'd4', 'd5'],
+        '1-env_IT1': ['it', 'i1']}
+    # Update default activation key with tiaa_env key
+    for activation_key, tiaa_grain in activation_key_map.items():
+        if __grains__["tiaa_env"] in tiaa_grain:
+            activation_keys[1] = activation_key
+
+    # Map tiaa_site to activation key segment
+    activation_key_map = {
+        '1-site_A': 'a',
+        '1-site_B': 'b',
+        '1-site_C': 'c',
+        '1-site_D': 'd',
+        '1-site_E': 'e'}
+    # Update default activation key with tiaa_env key
+    for activation_key, tiaa_grain in activation_key_map.items():
+        if __grains__["tiaa_site"] in tiaa_grain:
+            activation_keys[2] = activation_key
+
+    # Map tiaa_tier to activation key segment
+    activation_key_map = {
+        '1-tier_1': '1',
+        '1-tier_2': '2',
+        '1-tier_3': '3',
+        '1-tier_IS': 'i'}
+    # Update default activation key with tiaa_env key
+    for activation_key, tiaa_grain in activation_key_map.items():
+        if __grains__["tiaa_tier"] in tiaa_grain:
+            activation_keys[3] = activation_key
 
     return ' '.join(activation_keys)
