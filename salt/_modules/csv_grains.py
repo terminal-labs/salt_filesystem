a3fc811226738caf5f702e6025e6580bd5c6e212
Michael Pinna <mikepinna3@gmail.com>
2022-07-14 18:22:35 -0400

    test state

--- salt/_modules/csv_grains.py
+++ salt/_modules/csv_grains.py
@@ -16,8 +16,8 @@ def _get_saltenv():
 
 
 def get_diff(
-        yesterday_filepath="salt://state/csv_grains_updater/files/yesterday.csv",
-        today_filepath="salt://state/csv_grains_updater/files/today.csv"):
+        today_filepath="salt://state/csv_grains_updater/files/today.csv",
+        yesterday_filepath="salt://state/csv_grains_updater/files/yesterday.csv"):
 
     saltenv = _get_saltenv()
     diff = __salt__["file.get_diff"](
