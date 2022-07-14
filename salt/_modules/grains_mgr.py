f5297c2a2e148d39add4918ca5b182dcf48f068a
Michael Pinna <mikepinna3@gmail.com>
2022-07-14 14:19:34 -0400

    test state

--- salt/_modules/grains_mgr.py
+++ salt/_modules/grains_mgr.py
@@ -12,24 +12,22 @@ __virtualname__ = "grains_mgr"
 
 
 def patching_enabled():
-    tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
-    if tiaa_patching_grain is ("" or False):
-        __salt__['grains.setval']("tiaa_patching", True, destructive=False)
-        tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
-    return tiaa_patching_grain
+    tiaa_patching_enabled_grain = __salt__[
+        'grains.get']("tiaa_patching_enabled")
+    if tiaa_patching_enabled_grain is ("" or False):
+        __salt__['grains.setval'](
+            "tiaa_patching_enabled", True, destructive=False)
+        tiaa_patching_enabled_grain = __salt__[
+            'grains.get']("tiaa_patching_enabled")
+    return tiaa_patching_enabled_grain
 
 
 def patching_disabled():
-    tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
-    if tiaa_patching_grain is ("" or True):
-        __salt__['grains.setval']("tiaa_patching", False, destructive=False)
-        tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
-    return tiaa_patching_grain
-
-
-def remove_grain(grain_key="tiaa_patching"):
-    if __salt__['grains.get']("tiaa_patching") is "":
-        return f"No {grain_key} grain is present."
-    else:
-        __salt__['grains.delkey'](grain_key)
-        return f"Grain {grain_key} was deleted."
+    tiaa_patching_enabled_grain = __salt__[
+        'grains.get']("tiaa_patching_enabled")
+    if tiaa_patching_enabled_grain is ("" or True):
+        __salt__['grains.setval'](
+            "tiaa_patching_enabled", False, destructive=False)
+        tiaa_patching_enabled_grain = __salt__[
+            'grains.get']("tiaa_patching_enabled")
+    return tiaa_patching_enabled_grain
