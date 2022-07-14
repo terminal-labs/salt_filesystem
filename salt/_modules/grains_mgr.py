9e1a9cc3ad361577291077d259aaa162296f1606
Michael Pinna <mikepinna3@gmail.com>
2022-07-14 14:49:53 -0400

    test state

--- salt/_modules/grains_mgr.py
+++ salt/_modules/grains_mgr.py
@@ -12,22 +12,16 @@ __virtualname__ = "grains_mgr"
 
 
 def patching_enabled():
-    tiaa_patching_enabled_grain = __salt__[
-        'grains.get']("tiaa_patching_enabled")
-    if tiaa_patching_enabled_grain is ("" or False):
-        __salt__['grains.setval'](
-            "tiaa_patching_enabled", True, destructive=True)
-        tiaa_patching_enabled_grain = __salt__[
-            'grains.get']("tiaa_patching_enabled")
-    return tiaa_patching_enabled_grain
+    tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
+    if tiaa_patching_grain is ("" or False):
+        __salt__['grains.setval']("tiaa_patching", True)
+        tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
+    return tiaa_patching_grain
 
 
 def patching_disabled():
-    tiaa_patching_enabled_grain = __salt__[
-        'grains.get']("tiaa_patching_enabled")
-    if tiaa_patching_enabled_grain is ("" or True):
-        __salt__['grains.setval'](
-            "tiaa_patching_enabled", False, destructive=True)
-        tiaa_patching_enabled_grain = __salt__[
-            'grains.get']("tiaa_patching_enabled")
-    return tiaa_patching_enabled_grain
+    tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
+    if tiaa_patching_grain is ("" or True):
+        __salt__['grains.setval']("tiaa_patching", False)
+        tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
+    return tiaa_patching_grain
