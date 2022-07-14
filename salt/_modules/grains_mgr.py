689d6e02ae22f8ba025bbf6da19c251755d3cab7
Michael Pinna <mikepinna3@gmail.com>
2022-07-14 16:51:32 -0400

    test state

--- salt/_modules/grains_mgr.py
+++ salt/_modules/grains_mgr.py
@@ -13,7 +13,7 @@ __virtualname__ = "grains_mgr"
 
 def patching_enabled():
     tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
-    if ("" or False) is tiaa_patching_grain:
+    if tiaa_patching_grain == ("" or False):
         __salt__['grains.setval']("tiaa_patching", True)
         tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
     return tiaa_patching_grain
@@ -21,7 +21,7 @@ def patching_enabled():
 
 def patching_disabled():
     tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
-    if ("" or True) is tiaa_patching_grain:
+    if tiaa_patching_grain == ("" or True):
         __salt__['grains.setval']("tiaa_patching", False)
         tiaa_patching_grain = __salt__['grains.get']("tiaa_patching")
     return tiaa_patching_grain
