083a4744d14daf4e3331ed6bd663ea4dd003e2d9
Michael Pinna <mikepinna3@gmail.com>
2022-07-14 18:05:11 -0400

    test state

--- salt/_modules/csv_grains.py
+++ salt/_modules/csv_grains.py
@@ -13,3 +13,16 @@ __virtualname__ = "csv_grains"
 
 def _get_saltenv():
     return __salt__["config.get"]("saltenv")
+
+
+def get_differences(
+        today_filepath="salt://state/csv_grains_updater/files/today.csv",
+        yesterday_filepath="salt://state/csv_grains_updater/files/yesterday.csv"):
+
+    saltenv = _get_saltenv()
+    differences = __salt__["file.get_diff"](
+        today_filepath,
+        yesterday_filepath,
+        saltenv=saltenv)
+
+    return type(differences)
