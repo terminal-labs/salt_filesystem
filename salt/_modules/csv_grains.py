ff81a37ecf8707e44e217fa994f0121686c9df93
Michael Pinna <mikepinna3@gmail.com>
2022-07-14 18:11:56 -0400

    test state

--- salt/_modules/csv_grains.py
+++ salt/_modules/csv_grains.py
@@ -15,14 +15,16 @@ def _get_saltenv():
     return __salt__["config.get"]("saltenv")
 
 
-def get_differences(
+def get_diff(
         today_filepath="salt://state/csv_grains_updater/files/today.csv",
         yesterday_filepath="salt://state/csv_grains_updater/files/yesterday.csv"):
 
     saltenv = _get_saltenv()
-    differences = __salt__["file.get_diff"](
+    diff = __salt__["file.get_diff"](
         today_filepath,
         yesterday_filepath,
         saltenv=saltenv).split("\n")
 
-    return differences
+    diff = [line for line in diff if line[:2] != ("--", "++", "@@")]
+
+    return diff
