443c3ac671713f2fbbc708b5676d2d26d7d4a085
Michael Pinna <mikepinna3@gmail.com>
2022-07-15 13:30:25 -0400

    test state

--- salt/_modules/csv_grains.py
+++ salt/_modules/csv_grains.py
@@ -36,21 +36,21 @@ def delete_grains():
     subtractions = [line.split(',') for line in get_diff()['subtractions']]  # noqa:E501
     for subtraction in subtractions:
         if __grains__['id'] == subtraction[0][1:]:
-            tiaa_maintsched_deleted = __salt__[
-                'grains.delkey']("tiaa_maintsched")
-            tiaa_patching_deleted = __salt__['grains.delkey']("tiaa_patching")
-    return tiaa_maintsched_deleted, tiaa_patching_deleted
+            __salt__['grains.delkey']("tiaa_maintsched")
+            __salt__['grains.delkey']("tiaa_patching")
+
+    tiaa_maintsched = __salt__['grains.get']("tiaa_maintsched")
+    tiaa_patching = __salt__['grains.get']("tiaa_patching")
+    return {"tiaa_maintsched": tiaa_maintsched}, {"tiaa_patching": tiaa_patching}  # noqa:E501
 
 
 def create_grains():
     additions = [line.split(',') for line in get_diff()['additions']]  # noqa:E501
     for addition in additions:
         if __grains__['id'] == addition[0][1:]:
-            tiaa_maintsched_added = __salt__['grains.setval'](
-                "tiaa_maintsched", addition[2])
+            __salt__['grains.setval']("tiaa_maintsched", addition[2])
             if "app" in addition:
-                tiaa_patching_added = __salt__[
-                    'grains.setval']("tiaa_patching", True)
+                __salt__['grains.setval']("tiaa_patching", True)
 
     tiaa_maintsched = __salt__['grains.get']("tiaa_maintsched")
     tiaa_patching = __salt__['grains.get']("tiaa_patching")
