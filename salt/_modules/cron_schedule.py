8e15b57697085ec1f058ed3400ab04fc38376bb2
Michael Pinna <mikepinna3@gmail.com>
2022-07-16 11:53:41 -0400

    test

--- salt/_modules/cron_schedule.py
+++ salt/_modules/cron_schedule.py
@@ -13,32 +13,6 @@ from pprint import pprint
 # Module name
 __virtualname__ = "cron_schedule"
 
-#   case $week {
-#     '1st': {$wom = '1-7'}
-#     '2nd': {$wom = '8-14'}
-#     '3rd': {$wom = '15-21'}
-#     '4th': {$wom = '22-28'}
-# '1st&3rd': {$wom = ['1-7', '15-21']}
-# '2nd&4th': {$wom = ['8-14', '22-28']}
-#   'every': {$wom = '*'}
-#   default: { fail('Patch WeekofMonth is not supported') }
-#   }
-
-#   # resource title must not change: they are used by Puppet to match cron entries
-#   cron { 'Monthly maintenance: patch & restart instance':
-#     ensure   => 'present',
-#     command  => "[ $(date +\\%A) == '${day}' ] && /usr/local/scripts/monthlyMaint.sh",
-#     user     => 'root',
-#     month    => '*',
-#     monthday => $wom,
-#     weekday  => '*',
-#     hour     => "${hour}",
-#     minute   => fqdn_rand( 50 ),
-#   }
-#   }
-# 42 21 8-14,22-28 * *
-# min hour day * *
-
 
 def transform_tiaa_maintsched(tiaa_maintsched):
     cron_sched = None
@@ -61,8 +35,3 @@ def transform_tiaa_maintsched(tiaa_maintsched):
 
         cron_sched = dict(hour=hour, day=day, weekday=weekday)
     return cron_sched
-
-
-# pprint(transform_tiaa_maintsched("2nd&4th-Tuesday@9pm"))
-# pprint(transform_tiaa_maintsched("1st&3rd-Monday@9pm"))
-# pprint(transform_tiaa_maintsched("every-Wednesday@11pm"))
