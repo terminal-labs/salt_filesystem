0468ce1dd2ef3b4e70371f715c3c1108399f1021
Michael Pinna <mikepinna3@gmail.com>
2022-07-19 09:25:17 -0400

    test

--- salt/state/cron_patch/files/patching_script.ps1
+++ salt/state/cron_patch/files/patching_script.ps1
@@ -1,2 +1,3 @@
 New-Item "C:\Users\Administrator\patching_script_worked.txt"
 Set-Content "C:\Users\Administrator\patching_script_worked.txt" "I run @ {{hour}} on {{weekday}} but only the {{monthday}}."
+{Exit 0}
